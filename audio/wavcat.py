233a511074655c81738da919f896b2d71789b6ea
svilen <svilen.dobrev@ericsson.com>
2013-12-04 17:08:50 +0000

    fix

--- audio/wavcat.py
+++ audio/wavcat.py
@@ -5,7 +5,7 @@ from util import optz
 optz.usage( '%prog [opts] wavfile[s]' )
 optz.bool( 'verbose', '-v')
 optz.text( 'outfile', '-o', help= 'outfile, "-" for stdout')
-optz.int(  'maxsize', '-l', help= 'max size in MBs')
+optz.int(  'maxsize', '-l', help= 'max size in MBs, per item')
 options,args = optz.get()
 
 import wave
@@ -14,13 +14,18 @@ oname = options.outfile
 if oname == '-': oname = sys.stdout
 sz = options.maxsize
 totals = 0.0
+oparams = None
 for a in args:
     i = wave.open( a, 'r')
     params = (nchannels, sampwidth, framerate, nframes, comptype, compname) = i.getparams()
+    iparams = params[:3]+params[4:]
     size = nframes/float(framerate)
     totals += size
     print >>sys.stderr, '< %s: %.2f s' % (a, size,) ,
     print >>sys.stderr, options.verbose and params or ''
+    if oparams:
+        assert iparams == oparams, (oparams, iparams)
+    oparams = iparams
     if not oname: continue
     while 1:
         data = i.readframes(1024*1024)
