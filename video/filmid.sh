79f8406bbdbbb4cd8ce6ab79f3a4b2f65302c5c3
svilen <svilen@dom>
2014-11-04 23:05:07 +0200

    sync

--- video/filmid.sh
+++ video/filmid.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 #|Genre
-mplayer -vo null -ao null -frames 0 -msglevel identify=4 "$@" 2>/dev/null | grep -E '((VIDEO|AUDIO|Title|Artist|Album|Year|Comment): *[^ ]+|Playing|ID_(LENG|VIDEO_[HW])|Track|[sa]lang|subtitle|^VIDEO )'
+#mplayer -vo null -ao null -frames 0 -msglevel identify=4 "$@" 2>/dev/null | grep -E '((VIDEO|AUDIO|Title|Artist|Album|Year|Comment): *[^ ]+|Playing|ID_(LENG|VIDEO_[HW])|Track|[sa]lang|subtitle|^VIDEO )'
+for a in "$@"; do ffprobe -hide_banner "$a"; done 2>& 1
