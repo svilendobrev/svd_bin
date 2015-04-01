17027ad5cee14ec4793b7320b3e8a780f779a9d4
svilen <svilen@dom>
2015-04-02 00:32:57 +0300

    f

--- misc/lih.py
+++ misc/lih.py
@@ -11,6 +11,6 @@ mes_lihva = god_procent /100.0/12
 x= (1 + mes_lihva) ** meseci
 mes_suma = (suma * x * mes_lihva)/(x-1)
 vse_suma = mes_suma * meseci
-print( mes_suma, '/m', vse_suma, int(vse_suma*100/suma),'%')
+print( int(mes_suma), '/m', int(vse_suma), int(vse_suma*100/suma),'%')
 
 # vim:ts=4:sw=4:expandtab
