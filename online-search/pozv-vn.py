ddb9cb839b328aa1a51761c27885ff08e2caf2be
svilen <svilen@dom>
2016-10-06 20:10:56 +0300

    updat

---
+++ online-search/pozv-vn.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function #,unicode_literals
+from pozvanete import optz, main
+info_includes = 'ВАРНА'
+text_excludes = '''
+БОКСОНИЕРА
+ДВУСТАЕН ДВУСТАЙНИ
+ЕДНОСТАЕН
+^ПОМЕЩЕНИЕ
+ГАРСОНИЕРА ГАРСОНИЕРИ
+2-СТАЕН
+
+^ПАРЦЕЛ
+^ДВОР
+^МЯСТО
+^ГАРАЖ
+^ПОДЗЕМЕН
+^ОВОЩНА
+ЗЕМЕДЕЛСКА ЗЕМЕДЕЛСКИ
+НИВА
+^ЛОЗЕ
+^ПАРТЕРНО
+ВИЛА
+
+ФИРМА
+ЗАВЕДЕНИЕ
+СЛАДКАРНИЦА
+^КАФЕ
+МАГАЗИН
+РАБОТЕЩ
+ТУРИСТИЧЕСКА
+цех
+^МАСИВЕН
+^МАСИВНА
+ИНДУСТРИАЛЕН
+ПРОМИШЛЕН
+ПРОМИШЛЕНА
+ОБОРУДВАН
+ОФИС
+^АТЕЛИЕ
+хале
+халета
+СКЛАДОВЕ
+
+www.karavellagreenhouse.com
+
+с.
+гр.
+
+с.Каменар
+Кранево
+
+'''
+
+if 1:
+    main( info_includes=info_includes, text_excludes=text_excludes,)
+
+# vim:ts=4:sw=4:expandtab
