0cd245ae10744d8569e784608bc3c6ca25db4e34
svilen <svilen@email>
2019-03-24 14:35:00 +0200

    upd

---
+++ misc/exitmenu.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+scelta=$( Xdialog --title " exit?" --shadow --menu "?" 17 35 35 \
+      "q" "poweroff"  \
+      "x" "xrestart"  \
+      "r" "reboot"    \
+      2>&1 );
+
+case $scelta in
+x) killall nodm ;;
+q) systemctl poweroff ;;
+r) systemctl reboot ;;
+#h) systemctl hibernate ;;
+#s) systemctl suspend   ;;
+*) echo "invalid result ($scelta) "; exit;;
+esac
+
+# vim:ts=4:sw=4:expandtab
